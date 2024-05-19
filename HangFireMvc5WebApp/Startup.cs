957982ce293bd80ac38f737f144f56b4e71bebbb
Nguyen Viet Manh <manh.nguyen@thirdsight.net>
2024-05-19 15:35:04 +0800

    Rename

---
+++ HangFireMvc5WebApp/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(HangFireMvc5WebApp.Startup))]
+namespace HangFireMvc5WebApp
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
